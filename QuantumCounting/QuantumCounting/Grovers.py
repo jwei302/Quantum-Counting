3283e17c30c06fc431a46deafc05d5621f72667d
shuhul <shuhulmujoo@gmail.com>
2021-07-22 07:53:52 -0700

    Created Grovers

--- QuantumCounting/QuantumCounting/Grovers.py
+++ QuantumCounting/QuantumCounting/Grovers.py
@@ -1,1 +1,18 @@
+import qiskit;
 
+
+
+def FlipPhaseIfAllZeros(circuit, register, target):
+    circuit.x(register)
+    cz_gate = ZGate().control(len(register))
+    circuit.append(cz_gate,register+target)
+    circuit.x(register)
+
+def DiffusionOperator(circuit, register, target):
+    circuit.h(register)
+    FlipPhaseIfAllZeros(circuit,register,target)
+    circuit.h(register)
+
+def GroverIteration(circuit, input, target, oracle):
+    oracle(circuit,input,target)
+    DiffusionOperator(circuit,input,target)
